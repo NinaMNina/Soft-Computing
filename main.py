789a7f868919a1828642c1a441e8328178ebd80c
Nina <ninamiladinovic@hotmail.com>
2018-01-28 21:48:12 +0100

    dodat interfejs

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import math
 import collections
 import os
 
+import app
+
 try:
     from cv2 import cv2
 except ImportError:
     pass
 
 if __name__ == '__main__':
+    window = app.__init__()
     print 'hi'
     img = cv2.imread('images/ops.jpg')
     # image_ada = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
