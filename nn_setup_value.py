8d9a55053ad56efe5f29a22f13acd23c93c8ce7c
tanjaindjic <mali.patuljko@gmail.com>
2018-02-01 17:51:39 +0100

    dodato prepoznavanje duzine pauza u 2. neuronsku ali radi traljavo

--- nn_setup_value.py
+++ nn_setup_value.py
@@ -8,7 +8,7 @@ from keras.layers import MaxPooling2D, Activation, Dropout
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 
-nClasses = 5
+nClasses = 10
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(70,30,3)))
@@ -57,236 +57,78 @@ batch_size = 32,
 class_mode = 'categorical')
 
 model.fit_generator(training_set,
-steps_per_epoch = 370,
+steps_per_epoch = 902,
 epochs = 2,
 validation_data = test_set,
-validation_steps = 71)
+validation_steps = 488)
+
+def checkLength(path):
+    img = cv2.imread(path)
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.bitwise_not(gray_image)
+    cv2.imwrite('bitwtest.png', img)
+    test_image = load_img('bitwtest.png', target_size=(70, 30))
+    test_image = img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
+    result = model.predict(test_image)
 
 
-# Part 3 - Making new predictions
-img = cv2.imread('images/predict/test1.png')
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img =  cv2.bitwise_not(gray_image)
-cv2.imwrite('bitwtest.png', img)
-test_image = load_img('bitwtest.png', target_size = (70, 30))
-test_image = img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict(test_image)
-print ("should be 1-4")
-
-print (result)
-resMax = result[0][0]
-pred = '1-1'
-if resMax < result [0][1]:
-    resMax = result [0][1]
-    pred = '1-2'
-if resMax < result [0][2]:
-    resMax = result [0][2]
-    pred = '1-4'
-if resMax < result [0][3]:
-    resMax = result [0][3]
-    pred = '1-8'
-if resMax < result[0][4]:
-    resMax = result[0][4]
-    pred = '1-16'
-print (resMax, pred)
-
-
-# Part 3 - Making new predictions
-img = cv2.imread('images/predict/test2.png')
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img =  cv2.bitwise_not(gray_image)
-cv2.imwrite('bitwtest.png', img)
-test_image = load_img('bitwtest.png', target_size = (70, 30))
-test_image = img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict(test_image)
-print ("should be 1-4")
-
-print (result)
-resMax = result[0][0]
-pred = '1-1'
-if resMax < result [0][1]:
-    resMax = result [0][1]
-    pred = '1-2'
-if resMax < result [0][2]:
-    resMax = result [0][2]
-    pred = '1-4'
-if resMax < result [0][3]:
-    resMax = result [0][3]
-    pred = '1-8'
-if resMax < result[0][4]:
-    resMax = result[0][4]
-    pred = '1-16'
-print (resMax, pred)
-
+    print (result)
+    resMax = result[0][0]
+    pred = 'n1-1'
+    if resMax < result[0][1]:
+        resMax = result[0][1]
+        pred = 'n1-16'
+    if resMax < result[0][2]:
+        resMax = result[0][2]
+        pred = 'n1-2'
+    if resMax < result[0][3]:
+        resMax = result[0][3]
+        pred = 'n1-4'
+    if resMax < result[0][4]:
+        resMax = result[0][4]
+        pred = 'n1-8'
+    if resMax < result[0][5]:
+        resMax = result[0][5]
+        pred = 'p1-1'
+    if resMax < result[0][6]:
+        resMax = result[0][6]
+        pred = 'p1-16'
+    if resMax < result[0][7]:
+        resMax = result[0][7]
+        pred = 'p1-2'
+    if resMax < result[0][8]:
+        resMax = result[0][8]
+        pred = 'p1-4'
+    if resMax < result[0][9]:
+        resMax = result[0][9]
+        pred = 'p1-8'
+    print (resMax, pred)
 
 # Part 3 - Making new predictions
-img = cv2.imread('images/predict/test3.png')
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img =  cv2.bitwise_not(gray_image)
-cv2.imwrite('bitwtest.png', img)
-test_image = load_img('bitwtest.png', target_size = (70, 30))
-test_image = img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict(test_image)
-print ("should be 1-4")
-
-print (result)
-resMax = result[0][0]
-pred = '1-1'
-if resMax < result [0][1]:
-    resMax = result [0][1]
-    pred = '1-2'
-if resMax < result [0][2]:
-    resMax = result [0][2]
-    pred = '1-4'
-if resMax < result [0][3]:
-    resMax = result [0][3]
-    pred = '1-8'
-if resMax < result[0][4]:
-    resMax = result[0][4]
-    pred = '1-16'
-print (resMax, pred)
 
 
-# Part 3 - Making new predictions
-img = cv2.imread('images/predict/test4.png')
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img =  cv2.bitwise_not(gray_image)
-cv2.imwrite('bitwtest.png', img)
-test_image = load_img('bitwtest.png', target_size = (70, 30))
-test_image = img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict(test_image)
-print ("should be 1-4")
-
-print (result)
-resMax = result[0][0]
-pred = '1-1'
-if resMax < result [0][1]:
-    resMax = result [0][1]
-    pred = '1-2'
-if resMax < result [0][2]:
-    resMax = result [0][2]
-    pred = '1-4'
-if resMax < result [0][3]:
-    resMax = result [0][3]
-    pred = '1-8'
-if resMax < result[0][4]:
-    resMax = result[0][4]
-    pred = '1-16'
-print (resMax, pred)
-
-
-# Part 3 - Making new predictions
-img = cv2.imread('images/predict/test5.png')
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img =  cv2.bitwise_not(gray_image)
-cv2.imwrite('bitwtest.png', img)
-test_image = load_img('bitwtest.png', target_size = (70, 30))
-test_image = img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict(test_image)
-print ("should be 1-4")
-
-print (result)
-resMax = result[0][0]
-pred = '1-1'
-if resMax < result [0][1]:
-    resMax = result [0][1]
-    pred = '1-2'
-if resMax < result [0][2]:
-    resMax = result [0][2]
-    pred = '1-4'
-if resMax < result [0][3]:
-    resMax = result [0][3]
-    pred = '1-8'
-if resMax < result[0][4]:
-    resMax = result[0][4]
-    pred = '1-16'
-print (resMax, pred)
-
-# Part 3 - Making new predictions
-img = cv2.imread('images/predict/1-2.png')
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img =  cv2.bitwise_not(gray_image)
-cv2.imwrite('bitwtest.png', img)
-test_image = load_img('bitwtest.png', target_size = (70, 30))
-test_image = img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict(test_image)
-print ("should be 1-2")
-
-print (result)
-resMax = result[0][0]
-pred = '1-1'
-if resMax < result [0][1]:
-    resMax = result [0][1]
-    pred = '1-2'
-if resMax < result [0][2]:
-    resMax = result [0][2]
-    pred = '1-4'
-if resMax < result [0][3]:
-    resMax = result [0][3]
-    pred = '1-8'
-if resMax < result[0][4]:
-    resMax = result[0][4]
-    pred = '1-16'
-print (resMax, pred)
-
-# Part 3 - Making new predictions
-img = cv2.imread('images/predict/1-16.png')
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img =  cv2.bitwise_not(gray_image)
-cv2.imwrite('bitwtest.png', img)
-test_image = load_img('bitwtest.png', target_size = (70, 30))
-test_image = img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict(test_image)
-print ("should be 1-16")
-
-print (result)
-resMax = result[0][0]
-pred = '1-1'
-if resMax < result [0][1]:
-    resMax = result [0][1]
-    pred = '1-2'
-if resMax < result [0][2]:
-    resMax = result [0][2]
-    pred = '1-4'
-if resMax < result [0][3]:
-    resMax = result [0][3]
-    pred = '1-8'
-if resMax < result[0][4]:
-    resMax = result[0][4]
-    pred = '1-16'
-print (resMax, pred)
-
-# Part 3 - Making new predictions
-img = cv2.imread('images/predict/a4_test.png')
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img =  cv2.bitwise_not(gray_image)
-cv2.imwrite('bitwtest.png', img)
-test_image = load_img('bitwtest.png', target_size = (70, 30))
-test_image = img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict(test_image)
-print ("should be 1-8")
-
-print (result)
-resMax = result[0][0]
-pred = '1-1'
-if resMax < result [0][1]:
-    resMax = result [0][1]
-    pred = '1-2'
-if resMax < result [0][2]:
-    resMax = result [0][2]
-    pred = '1-4'
-if resMax < result [0][3]:
-    resMax = result [0][3]
-    pred = '1-8'
-if resMax < result[0][4]:
-    resMax = result[0][4]
-    pred = '1-16'
-print (resMax, pred)
+checkLength('images/predict/1-2.png')
+print('---should be n1-2---')
+checkLength('images/predict/1-16.png')
+print('---should be n1-16---')
+checkLength('images/predict/a3.jpg')
+print('---should be n1-2---')
+checkLength('images/predict/a4_test.png')
+print('---should be n1-8---')
+checkLength('images/predict/cela_pauza.png')
+print('---should be p1-1---')
+checkLength('images/predict/cetvrtina_pauze.png')
+print('---should be p1-4---')
+checkLength('images/predict/d4.png')
+print('---should be n1-4---')
+checkLength('images/predict/len1-4.png')
+print('---should be n1-4---')
+checkLength('images/predict/osmina_pauze.png')
+print('---should be p1-8---')
+checkLength('images/predict/pause1-2.png')
+print('---should be p1-2---')
+checkLength('images/predict/pause1-4.png')
+print('---should be p1-4---')
+checkLength('images/predict/pola_pauze.png')
+print('---should be p1-2---')
